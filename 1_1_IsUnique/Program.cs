707df1bc1ef68c7d8dbc101bea56c0a6072b38bf
danruziska <danilo.ruziska@agilecontent.com>
2018-10-16 19:23:04 -0300

    First commit

---
+++ 1_1_IsUnique/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_IsUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "1s2#A!";
+            string str2 = "!s1B!";
+            string str3 = "aAbc?";
+            string str4 = "aA!sA#";
+
+
+            Console.WriteLine("IsUnique1");
+            Console.WriteLine(IsUnique1(str1));
+            Console.WriteLine(IsUnique1(str2));
+            Console.WriteLine(IsUnique1(str3));
+            Console.WriteLine(IsUnique1(str4));
+
+            Console.WriteLine("IsUnique2");
+            Console.WriteLine(IsUnique2(str1));
+            Console.WriteLine(IsUnique2(str2));
+            Console.WriteLine(IsUnique2(str3));
+            Console.WriteLine(IsUnique2(str4));
+        }
+
+        //Usando estrutura de dados
+        //Tempo de criação: 05:03:568
+        public static bool IsUnique1(string str)
+        {
+            Dictionary<char, bool> characters = new Dictionary<char, bool>();
+
+            foreach (var character in str)
+            {
+                if (characters.ContainsKey(character))
+                    return false;
+                characters.Add(character, true);
+            }
+
+            return true;
+        }
+
+        //Sem usar estrutura de dados
+        //Tempo de criação: 15:45:074
+        public static bool IsUnique2(string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Sort<char>(chars);
+
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[i + 1])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1_2_Permutation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "abc";
+            string str2 = "cba";
+
+            string str3 = "abc";
+            string str4 = "adb";
+
+            Console.WriteLine(isPermutation(str1,str2));
+            Console.WriteLine(isPermutation(str3,str4));
+        }
+
+        public static bool isPermutation(string a, string b)
+        {
+            char[] charsA = a.ToCharArray();
+            char[] charsB = b.ToCharArray();
+
+            Array.Sort<char>(charsA);
+            Array.Sort<char>(charsB);
+
+            return new string(charsA) == new string(charsB);
+        }
+    }
+}
